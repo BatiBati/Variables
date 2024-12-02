187f76033ddb9a6a95b00666c541b032c91ff39c
BatiBati <94386708+BatiBati@users.noreply.github.com>
2024-12-02 18:21:01 +0800

    Bodlogo bodov

--- index.js
+++ index.js
@@ -18,6 +18,81 @@ const hayag = "UB";
 const nas = "30";
 console.log(ovog, ner, gerlelt, hayag, hayag, nas);
 
+
+//True False//
+const number =1
+const stringNumber = "1"
+console.log (number === stringNumber)
+console.log (number == stringNumber)
+
+//Gurvaljingiin talbai//
+const aTal = "20";
+const bTal = "10";
+const cTal = "40";
+console.log (aTal * bTal * cTal)
+
+//m = y2-y1/x2-x1//
+const y2 = 10
+const y1 = 5
+console.log (y2-y1)
+
+const x2 = 20
+const x1 = 25
+console.log (x2-x1)
+
+console.log (x2-x1 / y2-y1)
+
+
+//huvaah uildel//
+const q2 = 50
+const q1 = 25
+console.log (q2 / q1)
+
+// 12. side1, side2, side3 гэсэн хувьсагчдад гурвалжны гурван талыг онооно.
+
+const side1 = "20";
+const side2 = "50";
+const side3 = "30";
+console.log (side1, side2, side3)
+
+//// 13. Өгсөн тоог цельсээс фаренгейт рүү хөрвүүл.
+
+const tsels = "20";
+console.log ((tsels * 1.8)+32);
+
+// 14. Хоёр багийн дундаж оноог олно уу. 
+//     ( A team - 96, 108, 89; B team - 88, 91, 110 )
+const aTeam96 = "96";
+const aTeam108 = "108";
+const aTeam89 = "89";
+
+const bTeam88 = "88";
+const bTeam91 = "91";
+const bTeam110 = "110";
+console.log (((aTeam96 + aTeam108 + aTeam89) / 3), ((bTeam88 + bTeam91 + bTeam110) / 3));
+
+// 15. 2 хувьсагч зарлан 2 тоон утга өг. Тухайн 2 
+//  хувьсагчдаар Math-ийн 5н үйлдэлүүдийг (+-*/%) гүцэтгээд үр дүнг нь дараах байдлаар харуул:
+// Өгөгдсөн 2 тооны нийлбэр нь 25
+// Өгөгдсөн 2 тооны ялгавар нь 5
+
+const firstNumber = "200";
+const secondNumber = "10";
+const niilber = "210";
+const yalgavar = "Yalgavar n:";
+const urjver = "2000";
+const noogdvor = "20";
+console.log (firstNumber + secondNumber);
+console.log (yalgavar, firstNumber - secondNumber);
+console.log (firstNumber * secondNumber);
+console.log (firstNumber / secondNumber);
+
+
+// 16. cm -г inch рүү хөрвүүлэх. inch -г cm рүү хөрвүүлэх
+
+
+
+
 //1. let
 //2. const
 //3. var
