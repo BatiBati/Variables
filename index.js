eb1ee21ba223f271e74c42eb46aa18481f1a9c72
BatiBati <94386708+BatiBati@users.noreply.github.com>
2024-12-02 17:22:05 +0800

    mt data

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+const firstName = "Bat";
+console.log(firstName);
+const lastName = "Dorj";
+console.log(lastName);
+const maritalStatus = "Married";
+console.log(maritalStatus);
+const country = "UB";
+console.log(country);
+const age = "20";
+console.log(age);
+
+const ovog = "Bat";
+const ner = "Dorj";
+const gerlelt = "tiim";
+const hayag = "UB";
+const nas = "30";
+console.log(ovog, ner, gerlelt, hayag, hayag, nas);
+
+//1. let
+//2. const
+//3. var
