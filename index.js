0c54b4b270be486527d3a2d9e816aaf7a2c6d1ac
Bati <94386708+BatiBati@users.noreply.github.com>
2024-12-20 09:05:57 +0800

    Condition Exercise 3 done

--- index.js
+++ index.js
@@ -82,74 +82,77 @@
 // console.log (yalgavar, firstNumber - secondNumber);
 // console.log (firstNumber * secondNumber);
 // console.log (firstNumber / secondNumber);
-
+// ----------------------------------Variable's exercise------------------------------------------
 // 1.
 // Comments can make code readable
 
 // 2.
-let numberVariable = 42;
-let stringVariable = "Hello, world!";
-let booleanVariable = true;
-let undefinedVariable;
-let nullVariable = null;
+// let numberVariable = 42;
+// let stringVariable = "Hello, world!";
+// let booleanVariable = true;
+// let undefinedVariable;
+// let nullVariable = null;
 
-console.log("Number: ", numberVariable);
-console.log("String: ", stringVariable);
-console.log("Boolean: ", booleanVariable);
-console.log("Undefined: ", undefinedVariable);
-console.log("Null: ", nullVariable);
+// console.log("Number: ", numberVariable);
+// console.log("String: ", stringVariable);
+// console.log("Boolean: ", booleanVariable);
+// console.log("Undefined: ", undefinedVariable);
+// console.log("Null: ", nullVariable);
 
 // 3.
-let myVariable;
-console.log(myVariable);
+// let myVariable;
+// console.log(myVariable);
 
 // 4.
-let myVariables = 10;
-console.log(myVariables);
+// let myVariables = 10;
+// console.log(myVariables);
 
 // 5.
 // First name, last name, marital status, country, age in many lines
-const firstName = "Bat";
-const lastName = "Naraa";
-const maritalStatus = "Yes";
-const country = "UB";
-const age = 20;
-console.log(firstName);
-console.log(lastName);
-console.log(maritalStatus);
-console.log(country);
-console.log(age);
+// const firstName = "Bat";
+// const lastName = "Naraa";
+// const maritalStatus = "Yes";
+// const country = "UB";
+// const age = 20;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(maritalStatus);
+// console.log(country);
+// console.log(age);
 
 // 6.
-console.log(firstName, lastName, maritalStatus, country, age);
+// console.log(firstName, lastName, maritalStatus, country, age);
 
 // 7.
-// Гурвалжингийн периметрийг олох
-const aTal = 50;
-const bTal = 8;
-const cTal = 10;
-let perimeter = aTal + bTal + cTal;
 
-console.log(perimeter);
+// ----------------------------------Operation exercise------------------------------------------
+// Гурвалжингийн периметрийг олох
+// const aTal = 50;
+// const bTal = 8;
+// const cTal = 10;
+// let perimeter = aTal + bTal + cTal;
+
+// console.log(perimeter);
 
 // 8. Тойргийн талбайг олох
-let radius = 3;
-let talbai = Math.PI * radius * radius;
+// let radius = 3;
+// let talbai = Math.PI * radius * radius;
 
-console.log(talbai);
+// console.log(talbai);
 
 // 9. m = y2-y1/x2-x1 хариуг олох
-const y1 = 3;
-const y2 = 15;
-const x1 = 2;
-const x2 = 5;
-let m = "";
-m = (y2 - y1) / (x2 - x1);
-console.log(m);
+// const y1 = 3;
+// const y2 = 15;
+// const x1 = 2;
+// const x2 = 5;
+// let m = "";
+// m = (y2 - y1) / (x2 - x1);
+// console.log(m);
 
 // 10. 15 МB дата 3 секундэд манай интернэт онлайнаар татаж чаддаг бол түүний BIT Rate-ийг олно уу.
 // const data = 15;
 // const seconds = 3;
+// console.log(data / seconds);
 
 // 11. side1, side2, side3 гэсэн хувьсагчдад гурвалжны гурван талыг онооно. Ийнхүү гурван тал өгөдсөн байхад үед гурвалжны талбайг ол.
 // const side1 = 10;
@@ -159,43 +162,203 @@ console.log(m);
 // 12. Өгсөн тоог цельсээс фаренгейт рүү хөрвүүл.
 
 // 13. Хоёр багийн дундаж оноог олно уу. ( A team - 96, 108, 89; B team - 88, 91, 110 )
-const aTeam96 = 96;
-const aTeam108 = 108;
-const aTeam89 = 89;
+// const aTeam96 = 96;
+// const aTeam108 = 108;
+// const aTeam89 = 89;
 
-const bTeam88 = 88;
-const bTeam91 = 91;
-const bTeam110 = 110;
+// const bTeam88 = 88;
+// const bTeam91 = 91;
+// const bTeam110 = 110;
 
-console.log((aTeam89 + aTeam108 + aTeam96) / 2);
-console.log((bTeam88 + bTeam91 + bTeam110) / 2);
+// console.log((aTeam89 + aTeam108 + aTeam96) / 2);
+// console.log((bTeam88 + bTeam91 + bTeam110) / 2);
 
-const teamA = "";
-const teamB = "";
-console.log((96 + 108 + 89) / 2, (88 + 91 + 110) / 2);
+// const teamA = "";
+// const teamB = "";
+// console.log((96 + 108 + 89) / 2, (88 + 91 + 110) / 2);
 
 // 14. 2 хувьсагч зарлан 2 тоон утга өг.
 // Тухайн 2 хувьсагчдаар Math-ийн 5н үйлдэлүүдийг (+-*/%) гүцэтгээд үр дүнг нь дараах байдлаар харуул:
 // Өгөгдсөн 2 тооны нийлбэр нь 25
 // Өгөгдсөн 2 тооны ялгавар нь 5
-const number1 = 32;
-const number2 = 10;
-console.log(`Өгөгдсөн 2 тооны нийлбэр нь: ${number1 + number2}`);
-console.log(`Өгөгдсөн 2 тооны ялгавар нь: ${number1 - number2}`);
-console.log(`Өгөгдсөн 2 тооны үржвэр нь: ${number1 * number2}`);
-console.log(`Өгөгдсөн 2 тооны ноогдвор нь: ${number1 * number2}`);
-console.log(`Өгөгдсөн 2 тооны ноогдвор нь: ${number1 % number2}`);
+// const number1 = 32;
+// const number2 = 10;
+// console.log(`Өгөгдсөн 2 тооны нийлбэр нь: ${number1 + number2}`);
+// console.log(`Өгөгдсөн 2 тооны ялгавар нь: ${number1 - number2}`);
+// console.log(`Өгөгдсөн 2 тооны үржвэр нь: ${number1 * number2}`);
+// console.log(`Өгөгдсөн 2 тооны ноогдвор нь: ${number1 * number2}`);
+// console.log(`Өгөгдсөн 2 тооны ноогдвор нь: ${number1 % number2}`);
 
 // 15. cm -г inch рүү хөрвүүлэх. inch -г cm рүү хөрвүүлэх
-const inch = 2.54;
-const cm = 20;
-console.log(`Инч - Сантиметр ${inch * cm}`);
-console.log(`Инч - Сантиметр ${cm / inch}`);
+// const inch = 2.54;
+// const cm = 20;
+// console.log(`Инч - Сантиметр ${inch * cm}`);
+// console.log(`Инч - Сантиметр ${cm / inch}`);
 
-// 16. 
+// 16.
+// let number = 345;  // Үр дүнг шалгах тоо
+// let firstDigit = Math.floor(number / 100);  // 100-ыг хувааж хамгийн эхний цифрийг авна
+// console.log(firstDigit);  // 3
 
+// let number = 345; // Үр дүнг шалгах тоо
+// let firstDigit = "" + number; // Тоог стринг болгон хөрвүүлж
+// firstDigit = firstDigit[0]; // Стрингийн хамгийн эхний тэмдэгтийг авна
+// console.log(firstDigit); // 3
 
+// ----------------------------------Condition exercise----------------------------------------------------------------------------------------------------------------------------
 
+// Exercise 1
+// <h3>Write a  which checks given input/parameter:</h3>
+// <ul>
+//   <li>If input/parameter is divisible by 3 print => Fizz</li>
+//   <li>If input/parameter is divisible by 5 print => Buzz</li>
+//   <li>If input/parameter is divisible by 3 or 5 print => FizzBuzz</li>
+//   <li>If input/parameter is NOT divisible by 3 or 5 print => given Input Number/Value</li>
+//   <li>If input/parameter is other than Number/Value print => 'Nan - Not a Number! Please Input Number' </li>
+// </ul>
+const parameter = "15";
+if (parameter % 3 === 0 && parameter % 5 === 0) {
+  console.log("FizzBuzz");
+} else if (parameter % 3 === 0) {
+  console.log("Fizz");
+} else if (parameter % 5 === 0) {
+  console.log("Buzz");
+} else if (isNaN(parameter)) {
+  console.log("Nan - Not a Number! Please Input Number");
+} else if (parameter % 3 !== 0 && parameter % 5 !== 0) {
+  console.log("given Input Number");
+}
 
+// Exercise 2
 
+// Write a JavaScript conditional statement to find the largest of five numbers.
+// check input numbers are the type of number, if not console.log('Invalid inputs');
 
+// Sample numbers : -5, -2, -6, 0, -1
+// Output : 0
+// const first = Number(prompt(`First number: `));
+// const second = Number(prompt(`second number: `));
+// const third = Number(prompt(`third number: `));
+// const fourth = Number(prompt(`fourth number: `));
+// const fifth = Number(prompt(`fifth number: `));
+// if (
+//   Number(isNaN(first)) ||
+//   Number(isNaN(second)) ||
+//   Number(isNaN(third)) ||
+//   Number(isNaN(fourth)) ||
+//   Number(isNaN(fifth))
+// ) {
+//     console.log("Toon utga oruulna uu!");
+// } else if (
+//   first > second &&
+//   first > third &&
+//   first > fourth &&
+//   first > fifth
+// ) {
+//     console.log(first);
+// }else if (
+//     second > first &&
+//     second > third &&
+//     second > fourth &&
+//     second > fifth
+//   ) {
+//       console.log(second);
+// }else if (
+//     third > first &&
+//     third > second &&
+//     third > fourth &&
+//     third > fifth
+//   ) {
+//       console.log(third);
+// }else if (
+//     fourth > first &&
+//     fourth > second &&
+//     fourth > third &&
+//     fourth > fifth
+//   ) {
+//       console.log(fourth);
+// }else if (
+//     fifth > first &&
+//     fifth > second &&
+//     fifth > third &&
+//     fifth > fourth
+// ) {
+//       console.log(fifth);
+//   }
+
+// Exercise 3
+
+// Write a JavaScript program that accept two integers and display the larger.
+
+// check input numbers are the type of number, if not console.log('Invalid inputs)
+
+// Sample numbers : 2,5
+// Output : 5
+const input1 = Number("asdas");
+const input2 = Number(7);
+
+if (Number.isNaN(input1) || Number.isNaN(input2)) {
+  console.log("Please enter number:");
+} else if (input1 > input2) {
+  console.log(input1);
+} else {
+  console.log(input2);
+}
+
+// Exercise 4
+
+// Write a JavaScript conditional statement to find the sign of product of three numbers.
+// Display an console.log box with the specified sign.
+
+// Sample numbers : 3, -7, 2
+// Output : The sign is -
+
+// Exercise 5
+
+// Choose the correct comparison operator to console.log true, when x is greater than y.
+// Choose the correct comparison operator to console.log false, when x is equal to y.
+// Choose the correct comparison operator to console.log not sure, when x is NOT equal to y.
+
+// Exercise 6
+
+// The Grade Assigner
+// - Write a if/else statement that:
+//   - takes 1 variable, a number score.
+//   - returns a grade for the score, either "A", "B", "C", "D", or "F".
+// - Call that if/else statement for a few different scores and log the result to make sure it works.
+
+// Exercise 10 j
+// Write If statement that takes string
+//     if string length is more than 10 print the string  has more than 10 characters
+//      if string length is more than 5 return the string has more than 5 characters
+//       if string length is less than 1 return the string has nothing
+//       if string length is equal to 1 return the string has 1 character
+// Exercise 7 j
+
+//  3) Write conditional expressions to satisfy the following safety rules:
+
+//  a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
+
+//  b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+
+//  c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+
+// Write If statement that takes number checks if the number is odd or even
+
+//     if odd return "the number is odd"
+//     if even return "the number is even"
+
+// Exercise 8 j
+
+// Write if statement that takes role and return each role with greeting
+
+//     if role is 'Employee' return 'Hello'
+//     if role is 'Director' return 'Greetings'
+//     if role is '' return 'Please provide role'
+//     else return 'Hi'
+
+// Exercise 8 j
+
+// 0. Гончигсумлай хоолны газар оров. Орсон хоолны газар нь 5000₮ - 30000₮ хооронд 15% нэмж төлдөг,
+// бусад үед 20% - ийг нэмж төлдөг бол. Эдгээр тохиолдолуудад нийт хэдийг төлөхийг олно уу.
+// Эдгээр тохиолдолуудад тус бүр хэдийг төлөх вэ? 3000₮, 27500₮, 100000₮.
